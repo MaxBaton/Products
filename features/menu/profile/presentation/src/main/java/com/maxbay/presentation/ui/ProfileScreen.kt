7da9e657f871267e191d19c949098439764f943d
MaxBaton <maks.bay.2015@mail.ru>
2024-02-06 14:13:37 +0300

    code refactor

--- features/menu/profile/presentation/src/main/java/com/maxbay/presentation/ui/ProfileScreen.kt
+++ features/menu/profile/presentation/src/main/java/com/maxbay/presentation/ui/ProfileScreen.kt
@@ -1,18 +1,10 @@
 package com.maxbay.presentation.ui
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import com.maxbay.presentation.R
 import com.maxbay.presentation.viewModel.ProfileContract
 import com.maxbay.productsTestEffectiveMobile.ui.ProductsTheme
 
